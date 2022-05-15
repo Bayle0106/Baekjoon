e4b68dbb8d25bec47bf51e29eae1acf5316698aa
DESKTOP-DH6M5AS\user <brayle0106@gmail.com>
2022-05-15 20:13:12 +0900

    list / iterator 사용법

---
+++ stack & list & queue/[5397] 키로거.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<list>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		string str;
+		cin >> str;
+		list<char> l;
+		list <char>::iterator it = l.begin();
+
+		for (char tmp : str)
+		{
+			switch (tmp)
+			{
+			case '<':
+				if (it != l.begin()) --it;
+				break;
+
+			case '>':
+				if (it != l.end()) ++it;
+				break;
+
+			case '-':
+				if (!l.empty() && it != l.begin())
+				{
+					--it;
+					l.erase(it);
+				}
+				break;
+
+			default:
+				// 현재 가리키는 iterator의 위치에 삽입 -> 만약 it가 end면 같이 이동하는 느낌
+				// -> 엄밀히는 end() 컨테이너를 가리키는 것이므로
+				l.insert(it, tmp);
+				break;
+			}
+		}
+
+		for (char tmp : l)
+			cout << tmp;
+		l.clear();
+		cout << "\n";
+	}
+}
+
